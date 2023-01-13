a71f1429dac70023299bf43ae43d015fee10b158
Katja Sabando <katja.sabando@gmail.com>
2023-01-13 14:27:03 +0100

    Collecting img file data

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+
+// Loading HTML using cheerio -> DONE
+// Scrape img links only -> DONE
+// Filter to 10 links -> download -> request data with fetch()
+// Push download to folder -> re-naming file name -> writing data to file
+
+try {
+  if (!fs.existsSync('./memes')) {
+    fs.mkdirSync('./memes');
+  }
+} catch (err) {
+  console.error(err);
+}
 
 axios
   .get('https://memegen-link-examples-upleveled.netlify.app/')
   .then((urlResponse) => {
     const $ = cheerio.load(urlResponse.data);
 
+    // for (let i = 0; i < 10; i++) {
     $('div').each((index, element) => {
-      const imageUrls = $(element).find('a').attr('href');
+      const imageUrls = $(element).find('img').attr('src');
 
-      console.log(imageUrls);
+      fetch(imageUrls).then((response) => {
+        const path = './memes/';
+
+        const dest = fs.createWriteStream(path);
+        response.body.pipe(dest);
+      });
     });
+
+    console.log('Images downloaded successfully!');
   });
